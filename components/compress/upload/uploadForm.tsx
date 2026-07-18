f5b5b18d8c4b3fc48ced337646dc0d4e3f66f2e5
Victor Santos <menezes.victorsantos@outlook.com>
2026-07-17 23:33:00 -0300

    feat: add basic error handling. Testing compression flow

--- components/compress/upload/uploadForm.tsx
+++ components/compress/upload/uploadForm.tsx
@@ -5,6 +5,9 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import Dropzone from "./dropzone"
 import { Presets } from "./presets"
 import { ensureSession } from "@/lib/session"
+import { toast } from "sonner"
+import { BetterAuthError } from "better-auth"
+import { Button } from "@/components/ui/button"
 
 
 export function UploadForm() {
@@ -15,33 +18,70 @@ export function UploadForm() {
         }
     })
     const onUpload = async (data: ICompressionForm) => {
-        await ensureSession()
-        const res = await fetch(`/api/compressor`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                contentType: data.file.type,
-                filename: data.file.name,
-                preset: data.preset,
-            }),
-            credentials: "include"
-        })
-        const { compressionId, uploadUrl, sourceKey } = await res.json()
-        const { ok } = await fetch(uploadUrl, {
-            method: "PUT",
-            headers: {
-                "Content-Type": data.file.type
-            },
-            body: data.file
-        })
+        try {
+            await ensureSession()
+            const res = await fetch(`/api/compressor`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    contentType: data.file.type,
+                    filename: data.file.name,
+                    preset: data.preset,
+                }),
+                credentials: "include"
+            })
+            if (!res.ok) {
+                throw new Error("Falha na conexão com o servidor.")
+            }
+            const { compressionId, uploadUrl, sourceKey } = await res.json()
+            const { ok } = await fetch(uploadUrl, {
+                method: "PUT",
+                credentials: "include",
+                headers: {
+                    "Content-Type": data.file.type
+                },
+                body: data.file
+            })
+            if (!ok) {
+                throw new Error("Falha ao salvar arquivo no servidor. Tente novamente mais tarde.")
+            }
+
+            const { ok: queued, statusText } = await fetch("/api/compressor/confirm-upload", {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    compressionId: compressionId
+                })
+            })
+            if (queued) {
+                toast.success(`${compressionId} adicionado à fila de compressão.`)
+            }
+            else {
+                toast.warning(statusText)
+            }
+        }
+        // TODO: Better error handling, doing it just for testing purposes
+        catch (err: any) {
+            toast.error(`${err.message}`)
+        }
     }
     return (
         <FormProvider {...methods}>
             <form onSubmit={methods.handleSubmit(onUpload)} className="flex flex-col gap-5 h-full ">
                 <Dropzone className="h-full" />
                 <Presets />
+                <Button onClick={async () => {
+                    const res = await fetch("/api/compressor", {
+                        method: "GET",
+                        credentials: "include"
+                    })
+                    console.log(await res.json())
+                }}>list</Button>
             </form>
         </FormProvider>)
 }
--- lib/session.ts
+++ lib/session.ts
@@ -3,8 +3,11 @@ import { authClient } from "./auth";
 // Checks if the user has session.
 // If not, creates an anonymous one.
 export async function ensureSession() {
-    const { data } = await authClient.getSession()
+    const { data, error: getSessionError } = await authClient.getSession()
     if (data?.session) return data
-    const { data: anonymous } = await authClient.signIn.anonymous()
+    const { data: anonymous, error } = await authClient.signIn.anonymous()
+    if (error || getSessionError) {
+        throw new Error("Falha ao validar acesso ao servidor. Tente novamente mais tarde.")
+    }
     return anonymous
 }
